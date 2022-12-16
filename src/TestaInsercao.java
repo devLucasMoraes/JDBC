6c74af72fcd628cd52e3f1e14c066791361a0ac2
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-15 22:28:52 -0300

    inserindo dados no banco

---
+++ src/TestaInsercao.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.recuperarConexao();
+
+        Statement statement = connection.createStatement();
+        statement.execute("INSERT INTO produtos (nome, descricao) VALUES ('mouse', 'mouse sem fio')", Statement.RETURN_GENERATED_KEYS);
+
+        ResultSet resultSet = statement.getGeneratedKeys();
+        while (resultSet.next()){
+            Integer id = resultSet.getInt(1);
+            System.out.println("O id criado foi: " + id);
+        }
+
+    }
+}

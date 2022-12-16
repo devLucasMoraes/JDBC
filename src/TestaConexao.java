5e8b0de8ca8573a6a7ea25826b14b578b56195b6
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-15 21:42:19 -0300

    criando conexao com o banco de dados

--- src/TestaConexao.java
+++ src/TestaConexao.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TestaConexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual","root","ht1943");
+
+        System.out.println("Fechando conexao");
+        connection.close();
+    }
 }
